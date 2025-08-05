be218036617b26b3efd7a274191e716f9bc4e0d6
VisPatrick <pksantos1003@gmail.com>
2025-08-05 19:36:36 -0300

    Conceitos

---
+++ aula28/exemplo1.py
@@ -0,0 +1,7 @@
+#   _BIG DATA SCIENCE_ANALISE DE DADOS
+#   Fundamentos da analise preditiva regressao linear, se baseia em dados historicos para prever o futuro
+#   Machine Learning - Aprendizado de Maquina, aprende atravez de dados
+#   Prendizado Supervisionado - dados com saida esperada tem o obejtivo de prever o futuro
+#   REGRESSAO LINEAR - FUNCAO DE PRIMEIRO GRAU F(Y)= aX + b
+#   NO PYTHON Y É A VARIAVEL DE DEPENDENCIA X É A VARIAVEL INDEPENDENTE, A coeficiente angular e B é o coeficiente linear
+#   FAZER UMA CORRELAÇÃO ANTES DE UMA ANALISE PREDITIVA, TEM QUE DAR UMA CORRELAÇÃO FORTE
